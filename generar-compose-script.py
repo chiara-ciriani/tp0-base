ab7e755cc97064d5da99a3ac134b82d7892963cc
chiara-ciriani <cciriani@fi.uba.ar>
2025-03-15 16:00:16 -0300

    ejercicio1: script generar-compose.sh

---
+++ generar-compose-script.py
@@ -0,0 +1,58 @@
+import sys
+import yaml
+
+def getComposeContentClient(client_id):
+  return {
+    'container_name': f'client{client_id}',
+    'image': 'client:latest',
+    'entrypoint': '/client',
+    'environment': [f'CLI_ID={client_id}', 'CLI_LOG_LEVEL=DEBUG'],
+    'networks': ['testing_net'],
+    'depends_on': ['server']
+  }
+
+def getComposeContent(clients_amount):
+  config = {
+    'name': 'tp0',
+    'services': {
+      'server': {
+        'container_name': 'server',
+        'image': 'server:latest',
+        'entrypoint': 'python3 /main.py',
+        'environment': ['PYTHONUNBUFFERED=1', 'LOGGING_LEVEL=DEBUG'],
+        'networks': ['testing_net']
+      }
+    },
+    'networks': {
+      'testing_net': {
+        'ipam': {
+          'driver': 'default',
+          'config': [{'subnet': '172.25.125.0/24'}]
+        }
+      }
+    } 
+  }
+
+  for i in range(1, clients_amount + 1):
+      new_client = getComposeContentClient(i)
+      config['services'][f'client{i}'] = new_client
+
+  return config
+
+def generate_docker_compose(output_file, clients_amount):
+  compose_content = getComposeContent(clients_amount)
+  
+  with open(output_file, 'w') as f:
+     yaml.dump(compose_content, f, default_flow_style=False, sort_keys=False)
+
+def main():
+  if len(sys.argv) != 3 or not sys.argv[2].isnumeric():
+    print("Incorrect parameters. The correct use is: ./generar-compose.sh <output_file> <clients_amount>")
+    sys.exit(1)
+
+  output_file = sys.argv[1]
+  clients_amount = int(sys.argv[2])
+
+  generate_docker_compose(output_file, clients_amount)
+
+main()
