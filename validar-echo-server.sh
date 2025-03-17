77ed15e37308f996efb689f3bcfeae2dbde8c888
chiara-ciriani <cciriani@fi.uba.ar>
2025-03-16 22:06:26 -0300

    ejercicio3: script validar-echo-server.sh

---
+++ validar-echo-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PORT=12345
+MESSAGE="Hello world!"
+TIMEOUT=5
+SERVER_CONTAINER="server"
+
+RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "echo $MESSAGE | nc -w $TIMEOUT $SERVER_CONTAINER $PORT")
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
