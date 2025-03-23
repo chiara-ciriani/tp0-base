296257ad3c9acc293e77497a01a7dd34def0de9d
chiara-ciriani <cciriani@fi.uba.ar>
2025-03-23 17:12:35 -0300

    test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -9,6 +9,8 @@ RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "echo $
 
 if [ "$RESPONSE" == "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
+    exit 0
 else
     echo "action: test_echo_server | result: fail"
+    exit 1
 fi
