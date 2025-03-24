428adb2c548b4878e5995b7e38eeb7413517fed1
chiara-ciriani <cciriani@fi.uba.ar>
2025-03-23 23:32:07 -0300

    ejercicio4: handle gracefully SIGTERM signal in client and server

--- server/common/server.py
+++ server/common/server.py
@@ -9,6 +9,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._client_socket = None
         self._down = False
 
         signal.signal(signal.SIGTERM, self.__handle_sigterm)
