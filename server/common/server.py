022eda63cbaef582c300bd46e0ebcfbb7faea5f6
chiara-ciriani <cciriani@fi.uba.ar>
2025-03-24 09:18:27 -0300

    ejercicio4: update README.md

--- server/common/server.py
+++ server/common/server.py
@@ -30,21 +30,21 @@ class Server:
                 self.__handle_client_connection()
             except OSError:
                 break
+
     def __handle_sigterm(self, signum, frame):
         logging.info('action: sigterm_received | result: in_progress')
 
-        logging.info('action: close_client_socket | result: in_progress')
         if self._client_socket:
-            self._client_socket.shutdown(socket.SHUT_RDWR)
+            logging.info('action: close_client_socket | result: in_progress')
             self._client_socket.close()
             self._client_socket = None
-        logging.info('action: close_client_socket | result: success')
+            logging.info('action: close_client_socket | result: success')
 
         logging.info('action: close_server_socket | result: in_progress')
-        self._down = True
         self._server_socket.shutdown(socket.SHUT_RDWR)
         self._server_socket.close()
         logging.info('action: close_server_socket | result: success')
+        self._down = True
 
         logging.info('action: sigterm_received | result: success')
 
